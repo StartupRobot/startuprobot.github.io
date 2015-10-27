5ac8bc133edaf30c3e286a719bf440f5c0d46d94
Winston Wilkins <winston@startuprobot.com>
2015-10-27 18:24:48 -0500

    added webhook to gitlab.

--- cloud/test/test.js
+++ cloud/test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../app.js');
 
 describe('GET /', function() {
-  it('should respond with hello Jenkins3', function(done) {
+  it('should respond with hello Jenkins3.', function(done) {
     request(app).get('/').expect('hello Jenkins3.', done);
   });
 });
