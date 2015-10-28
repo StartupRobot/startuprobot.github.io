703cdefd750ce3aa9cbea278619ed1b3d2194964
Winston Wilkins <winston@startuprobot.com>
2015-10-28 07:16:49 -0500

    adjusted webhook to gitlab to build on push.

--- cloud/test/test.js
+++ cloud/test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../app.js');
 
 describe('GET /', function() {
-  it('should respond with hello Jenkins4.', function(done) {
-    request(app).get('/').expect('hello Jenkins4.', done);
+  it('should respond with hello Jenkins5.', function(done) {
+    request(app).get('/').expect('hello Jenkins5.', done);
   });
 });
