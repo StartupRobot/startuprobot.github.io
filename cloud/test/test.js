86be7c14d6690bbcdf1c61a02c6007abe3d597bd
Winston Wilkins <winston@startuprobot.com>
2015-10-29 07:08:59 -0500

    took out extra repo in Jenkins configure.

--- cloud/test/test.js
+++ cloud/test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../app.js');
 
 describe('GET /', function() {
-  it('should respond with hello Jenkins9.', function(done) {
-    request(app).get('/').expect('hello Jenkins9.', done);
+  it('should respond with hello Jenkins10.', function(done) {
+    request(app).get('/').expect('hello Jenkins10.', done);
   });
 });
