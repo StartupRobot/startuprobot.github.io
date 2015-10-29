4e09f24943ecb099fb3e305f0434bc732c3c7a46
Winston Wilkins <winston@startuprobot.com>
2015-10-29 06:10:11 -0500

    Retyped login passsword for startJenkins in Jenkins configure.

--- cloud/app.js
+++ cloud/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('hello Jenkins7.');
+    res.send('hello Jenkins8.');
 });
 
 app.listen();
--- cloud/test/test.js
+++ cloud/test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../app.js');
 
 describe('GET /', function() {
-  it('should respond with hello Jenkins7.', function(done) {
-    request(app).get('/').expect('hello Jenkins7.', done);
+  it('should respond with hello Jenkins8.', function(done) {
+    request(app).get('/').expect('hello Jenkins8.', done);
   });
 });
