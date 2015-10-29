94d8ea87c8a50d6433579b17a2f82567bd5f6484
Winston Wilkins <winston@startuprobot.com>
2015-10-29 08:52:17 -0500

    Oshane made changes.

--- cloud/test/test.js
+++ cloud/test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../app.js');
 
 describe('GET /', function() {
-  it('should respond with hello Jenkins11.', function(done) {
-    request(app).get('/').expect('hello Jenkins11.', done);
+  it('should respond with hello Jenkins12.', function(done) {
+    request(app).get('/').expect('hello Jenkins12.', done);
   });
 });
