c9f17853d09dfb4d0ff74d06eb4fcc726444e5c7
Simantha Hong <simantha.hong@codecreators.co>
2015-11-29 20:20:56 -0500

    Initial commit of startuprobot.github.io

--- cloud/test/test.js
+++
@@ -1,8 +0,0 @@
-var request = require('supertest');
-var app = require('../app.js');
-
-describe('GET /', function() {
-  it('should respond with hello Jenkins12.', function(done) {
-    request(app).get('/').expect('hello Jenkins12.', done);
-  });
-});
